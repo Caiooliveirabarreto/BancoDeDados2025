271fb14f104f859529577340d1f653a792cdacad
Caio Oliveira Barreto <160022344+Caiooliveirabarreto@users.noreply.github.com>
2025-05-19 09:07:46 -0300

    Add files via upload

--- etecmcm2.sql
+++ etecmcm2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/05/2025 às 14:06
+-- Tempo de geração: 19/05/2025 às 14:07
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -61,34 +61,36 @@ CREATE TABLE `produtos` (
   `preco` decimal(10,2) DEFAULT NULL,
   `quant` int(11) DEFAULT NULL,
   `marca` varchar(255) DEFAULT NULL,
-  `id_categoria` int(11) DEFAULT NULL
+  `id_categoria` int(11) DEFAULT NULL,
+  `unidade_medida` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id_produto`, `nome`, `preco`, `quant`, `marca`, `id_categoria`) VALUES
-(1, 'Sabão em pó', 25.49, 4, 'OMO', 10),
-(3, 'Mussarela', 55.40, 100, 'President', 11),
-(4, 'Presunto', 60.35, 50, 'President', 11),
-(5, 'Iogurte Natural', 3.40, 60, 'Itambé', 3),
-(6, 'Leite', 6.49, 12, 'Paulista', 3),
-(7, 'Milho', 5.25, 6, 'Fugini', 5),
-(8, 'Ervilhas', 4.29, 4, 'Fugini', 5),
-(9, 'Acém', 42.75, 30, 'Perdigão', 4),
-(10, 'Picanha', 126.22, 20, 'Perdigão', 4),
-(11, 'Microondas', 450.99, 4, 'Samsung', 9),
-(12, 'Geladeira', 850.39, 2, 'Samsung', 9),
-(13, 'Coca-Cola', 3.49, 20, 'Coca-Cola', 7),
-(14, 'Detergente', 6.39, 10, 'Ypê', 10),
-(15, 'Cerveja', 5.50, 15, 'Skol', 7),
-(16, 'Papel Filme', 15.29, 5, 'Wyda', 12),
-(17, 'Papel Toalha', 12.99, 5, 'Snob', 12),
-(18, 'Alface', 6.99, 15, 'Frutelo', 6),
-(19, 'Maçã', 8.99, 12, 'Hortifruti', 6),
-(20, 'Perfume Masculino', 50.99, 4, 'Oboticário', 8),
-(21, 'Perfume Feminino', 90.99, 3, 'Jequiti', 8);
+INSERT INTO `produtos` (`id_produto`, `nome`, `preco`, `quant`, `marca`, `id_categoria`, `unidade_medida`) VALUES
+(1, 'Sabão em pó', 25.49, 4, 'OMO', 10, 'un'),
+(2, 'Mussarela', 63.45, 20, 'Sadia', 11, 'kg'),
+(3, 'Brócolis', 8.99, 30, 'Hortifruti', 11, 'Pacote 500'),
+(4, 'Presunto', 60.35, 50, 'President', 11, 'kg'),
+(5, 'Iogurte Natural', 3.40, 60, 'Itambé', 3, 'un'),
+(6, 'Leite', 6.49, 12, 'Paulista', 3, 'un'),
+(7, 'Milho', 5.25, 6, 'Fugini', 5, '300g'),
+(8, 'Ervilhas', 4.29, 4, 'Fugini', 5, '300g'),
+(9, 'Acém', 42.75, 30, 'Perdigão', 4, 'kg'),
+(10, 'Picanha', 126.22, 20, 'Perdigão', 4, 'kg'),
+(11, 'Microondas', 450.99, 4, 'Samsung', 9, 'un'),
+(12, 'Geladeira', 850.39, 2, 'Samsung', 9, 'un'),
+(13, 'Coca-Cola', 3.49, 20, 'Coca-Cola', 7, 'Lata 340g'),
+(14, 'Detergente', 6.39, 10, 'Ypê', 10, 'un'),
+(15, 'Cerveja', 5.50, 15, 'Skol', 7, 'Lata 340g'),
+(16, 'Papel Filme', 15.29, 5, 'Wyda', 12, 'un'),
+(17, 'Papel Toalha', 12.99, 5, 'Snob', 12, 'un'),
+(18, 'Alface', 6.99, 15, 'Frutelo', 6, 'kg'),
+(19, 'Maçã', 8.99, 12, 'Hortifruti', 6, 'kg'),
+(20, 'Perfume Masculino', 50.99, 4, 'Oboticário', 8, '200ml'),
+(21, 'Perfume Feminino', 90.99, 3, 'Jequiti', 8, '200ml');
 
 --
 -- Índices para tabelas despejadas
